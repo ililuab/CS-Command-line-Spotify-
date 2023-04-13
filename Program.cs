fdf018d34f49df7a156f5f78fac5a25a0f00d7f5
ililuab <1194420@student.roc-nijmegen.nl>
2023-04-13 12:49:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSEindopdracht__Command_line_Spotify_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
